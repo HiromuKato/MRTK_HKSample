28621a15203311e36fa5f9a7414f3fd0df722aa6
HiromuKato <hiromu1212@gmail.com>
2020-06-08 04:47:58 +0900

    HandPhotoCaptureのサンプル追加

---
+++ Assets/App/Scripts/HandPhotoCapture/HandPhotoCapture.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+namespace MRTK_HKSample
+{
+    /// <summary>
+    /// ハンドフォトキャプチャ
+    /// </summary>
+    public class HandPhotoCapture : MonoBehaviour
+    {
+        [SerializeField] 
+        private PhotoCaptureExample photoCapture = default;
+
+        [SerializeField]
+        private TextMesh leftDegreeText = default;
+
+        [SerializeField]
+        private TextMesh rightDegreeText = default;
+
+        [SerializeField]
+        private List<LineRenderer> lines = new List<LineRenderer>(4);
+
+        private IMixedRealityHandJointService handJointService = null;
+        private IMixedRealityDataProviderAccess dataProviderAccess = null;
+
+        void Start()
+        {
+            handJointService = CoreServices.GetInputSystemDataProvider<IMixedRealityHandJointService>();
+            if (handJointService == null)
+            {
+                Debug.LogError("Can't get IMixedRealityHandJointService.");
+                return;
+            }
+
+            dataProviderAccess = CoreServices.InputSystem as IMixedRealityDataProviderAccess;
+            if (dataProviderAccess == null)
+            {
+                Debug.LogError("Can't get IMixedRealityDataProviderAccess.");
+                return;
+            }
+
+            // ハンドレイを非表示にする
+            //PointerUtils.SetHandRayPointerBehavior(PointerBehavior.AlwaysOff);
+
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            foreach (var line in lines)
+            {
+                line.SetPosition(0, Vector3.zero);
+                line.SetPosition(1, Vector3.zero);
+            }
+            leftDegreeText.text = "0 degree";
+            rightDegreeText.text = "0 degree";
+        }
+
+        void Update()
+        {
+            // 線の太さ
+            foreach (var line in lines)
+            {
+                line.startWidth = 0.001f;
+                line.endWidth = 0.001f;
+            }
+
+            // 左手 人差し指
+            var leftIndexTip = handJointService.RequestJointTransform(TrackedHandJoint.IndexTip, Handedness.Left);
+            if (leftIndexTip == null)
+            {
+                Debug.Log("leftIndexTip is null.");
+                return;
+            }
+
+            // 左手 親指
+            var leftThumbTip = handJointService.RequestJointTransform(TrackedHandJoint.ThumbTip, Handedness.Left);
+            if (leftThumbTip == null)
+            {
+                Debug.Log("leftThumbTip is null.");
+                return;
+            }
+
+            // 左手 親指付け根
+            var leftThumbMetacarpal = handJointService.RequestJointTransform(TrackedHandJoint.ThumbMetacarpalJoint, Handedness.Left);
+            if (leftThumbMetacarpal == null)
+            {
+                Debug.Log("leftThumbMetacarpal is null.");
+                return;
+            }
+
+            // 左手の線描画
+            lines[0].SetPosition(0, leftIndexTip.position);
+            lines[0].SetPosition(1, leftThumbMetacarpal.position);
+            lines[1].SetPosition(0, leftThumbTip.position);
+            lines[1].SetPosition(1, leftThumbMetacarpal.position);
+
+            // 親指付け根を原点とした、人差し指と親指の角度算出
+            var p0 = leftThumbMetacarpal.position;
+            var p1 = leftIndexTip.position;
+            var p2 = leftThumbTip.position;
+            var v1 = p1 - p0;
+            var v2 = p2 - p0;
+            var angleLeft = Vector3.Angle(v2, v1);
+            leftDegreeText.text = angleLeft.ToString("0.0") + " degree";
+            leftDegreeText.transform.position = (leftIndexTip.position + leftThumbTip.position) / 2;
+
+            // 右手 人差し指
+            var rightIndexTip = handJointService.RequestJointTransform(TrackedHandJoint.IndexTip, Handedness.Right);
+            if (rightIndexTip == null)
+            {
+                Debug.Log("rightIndexTip is null.");
+                return;
+            }
+
+            // 右手 親指
+            var rightThumbTip = handJointService.RequestJointTransform(TrackedHandJoint.ThumbTip, Handedness.Right);
+            if (rightThumbTip == null)
+            {
+                Debug.Log("rightThumbTip is null.");
+                return;
+            }
+
+            // 右手 親指付け根
+            var rightThumbMetacarpal = handJointService.RequestJointTransform(TrackedHandJoint.ThumbMetacarpalJoint, Handedness.Right);
+            if (rightThumbMetacarpal == null)
+            {
+                Debug.Log("rightThumbMetacarpal is null.");
+                return;
+            }
+
+            // 右手の線描画
+            lines[2].SetPosition(0, rightIndexTip.position);
+            lines[2].SetPosition(1, rightThumbMetacarpal.position);
+            lines[3].SetPosition(0, rightThumbTip.position);
+            lines[3].SetPosition(1, rightThumbMetacarpal.position);
+
+            // 親指付け根を原点とした、人差し指と親指の角度算出
+            p0 = rightThumbMetacarpal.position;
+            p1 = rightIndexTip.position;
+            p2 = rightThumbTip.position;
+            v1 = p1 - p0;
+            v2 = p2 - p0;
+            var angleRight = Vector3.Angle(v2, v1);
+            rightDegreeText.text = angleRight.ToString("0.0") + " degree";
+            rightDegreeText.transform.position = (rightIndexTip.position + rightThumbTip.position) / 2;
+
+            if(CheckCondition(angleLeft, angleRight))
+            {
+                TakePhoto();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                TakePhoto();
+            }
+        }
+
+        /// <summary>
+        /// 写真撮影を開始するための条件となるアルゴリズム
+        /// とりあえず、左右の人差し指・親指の角度が共に60-90度の範囲になったらtrueを返す簡単な処理
+        /// 厳密に行うにはここの処理を工夫する
+        /// </summary>
+        /// <param name="angleLeft"></param>
+        /// <param name="angleRight"></param>
+        /// <returns></returns>
+        private bool CheckCondition(float angleLeft, float angleRight)
+        {
+            bool result = false;
+
+            if (60 < angleLeft && angleLeft < 90 &&
+                60 < angleRight && angleRight < 90)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        private void TakePhoto()
+        {
+            photoCapture.TakePhoto();
+        }
+    }
+}
---
+++ Assets/App/Scripts/HandPhotoCapture/PhotoCaptureExample.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using UnityEngine;
+using System.Linq;
+using System.Linq.Expressions;
+using UnityEngine.Windows.WebCam;
+
+namespace MRTK_HKSample
+{
+    /// <summary>
+    /// 写真撮影をするためのクラス
+    /// 参考：https://docs.unity3d.com/ja/2018.4/Manual/windowsholographic-photocapture.html
+    /// </summary>
+    public class PhotoCaptureExample : MonoBehaviour
+    {
+        [SerializeField] private TextMesh debug;
+
+        PhotoCapture photoCaptureObject = null;
+        Texture2D targetTexture = null;
+        private Resolution cameraResolution;
+
+        private bool photoTaking = false;
+
+        void Start()
+        {
+            cameraResolution = PhotoCapture.SupportedResolutions
+                .OrderByDescending((res) => res.width * res.height).First();
+            targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+        }
+
+        // 写真撮影する
+        public void TakePhoto()
+        {
+            if (photoTaking)
+            {
+                Debug.Log("写真撮影中");
+                return;
+            }
+            photoTaking = true;
+            debug.text = "写真撮影開始";
+
+            try
+            {
+                // PhotoCapture オブジェクトを作成します
+                PhotoCapture.CreateAsync(false, delegate(PhotoCapture captureObject)
+                {
+                    photoCaptureObject = captureObject;
+                    CameraParameters cameraParameters = new CameraParameters();
+                    cameraParameters.hologramOpacity = 0.0f;
+                    cameraParameters.cameraResolutionWidth = cameraResolution.width;
+                    cameraParameters.cameraResolutionHeight = cameraResolution.height;
+                    cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+                    // カメラをアクティベートします
+                    photoCaptureObject.StartPhotoModeAsync(cameraParameters,
+                        delegate(PhotoCapture.PhotoCaptureResult result)
+                        {
+                            // 写真を撮ります
+                            photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+                        });
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+                debug.text = ex.Message;
+            }
+        }
+
+        private void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+        {
+            // ターゲットテクスチャに RAW 画像データをコピーします
+            photoCaptureFrame.UploadImageDataToTexture(targetTexture);
+
+            // テクスチャが適用されるゲームオブジェクトを作成
+            GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
+            quadRenderer.material = new Material(Shader.Find("Mixed Reality Toolkit/Standard"));
+
+            Camera camera = Camera.main;
+            quad.transform.parent = this.transform;
+            //quad.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
+            quad.transform.localPosition = camera.transform.position + camera.transform.forward * 3;
+            quad.transform.localRotation = camera.transform.localRotation;
+
+            quadRenderer.material.SetTexture("_MainTex", targetTexture);
+
+            // カメラを非アクティブにします
+            photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+        }
+
+        private void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+        {
+            // photo capture のリソースをシャットダウンします
+            photoCaptureObject.Dispose();
+            photoCaptureObject = null;
+
+            photoTaking = false;
+            debug.text = "写真撮影終了";
+        }
+    }
+}
